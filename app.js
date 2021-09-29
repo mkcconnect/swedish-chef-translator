18c41108f350c0649ef8c21d1e1bd95eafa1e77a
sloth-grrr <35689080+sloth-grrr@users.noreply.github.com>
2021-09-29 20:48:46 +0530

    added app js

---
+++ app.js
@@ -0,0 +1,27 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL="https://api.funtranslations.com/translate/chef.json"
+
+function getTranslationURL (text){
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler (error){
+    console.log("Error occurred: ", error);
+    alert("Something is wrong. Try again later.")
+}
+
+function clickHandler() {
+    var inputText = txtInput.value;
+      fetch(getTranslationURL(inputText))
+         .then(response => response.json())
+             .then(json =>{
+                 var translated_text = json.contents.translated;
+                outputDiv.innerText = translated_text;
+             })
+             .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickHandler)
